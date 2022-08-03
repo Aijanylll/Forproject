fc227e3821a98d8db78ed171b832ba00c634cae7
Robert <you@example.com>
2022-08-03 17:37:06 +0600

    added fail

---
+++ 7.js
@@ -0,0 +1,99 @@
+// const films = ['Harry Potter', 'Supernatural', 'Creed II', 'Avengers']
+// console.log(films)
+// console.log(films[3])
+
+// films.push('Spiderman: no way home')
+// films.unshift('Doctor Strange')
+
+// console.log(films)
+
+// films.shift()
+// films.pop()
+
+// console.log(films)
+// console.log(films.length)
+
+// console.log(films.indexOf('Supernatural'))
+
+
+
+// const films = [
+//   ['Supernatural', 'Friends', 'lion'],
+//   [
+//     ['Avengers'],
+//     ['Home alone'],
+//     ['Harry Potter'],
+//   ]
+// ]
+
+// console.log(films[1][1][0])
+
+
+
+
+// const numbers = [1, 1, 2, 3, 5, 8]
+
+// const numbers2 = numbers.slice(3, 4)
+// console.log(numbers2)
+
+// const numbers3 = numbers.slice(7)
+// console.log(numbers3)
+
+// console.log(numbers)
+
+
+// const numbers = [1, 1, 2, 3, 5, 8]
+
+// const numbers4 = numbers.splice(4, 1)
+// console.log(numbers4)
+
+// const numbers5 = numbers.splice(0, 1, 0)
+// console.log(numbers5)
+// const number = [1, 1, 2, 3, 5, 8]
+
+// number.splice(4, 0, 5)
+// number.splice(2, 0, 1)
+
+// console.log(number)
+
+
+
+// const numbers = [1, 2, 3, 4, 5]
+// console.log(numbers)
+
+// numbers.reverse()
+// console.log(numbers)
+
+// numbers.reverse()
+// console.log(numbers)
+
+
+
+
+// Objects
+
+// const personArray = ['Adilet', 21, 'JS instructor']
+
+// const person = {
+//   name: 'Adilet',
+//   age: 21,
+//   job: 'JS instructor',
+//   'married status': 'not married',
+// //   marriedStatus: 'not married',
+//  }
+
+// console.log(person.name)
+// console.log(person['married status'])
+
+
+
+// const array = {
+//   length: 4,
+//   0: 'Adilet',
+//   1: 21,
+//   2: 'JS instructor',
+//   3: 'not married'
+// }
+
+// console.log(array[0])
+// console.log(array.length)
