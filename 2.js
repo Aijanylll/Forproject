491c066495d00d8b9d7544175b09cf131f089c9e
Robert <you@example.com>
2022-08-08 16:58:05 +0600

    added 1 file

---
+++ 2.js
@@ -0,0 +1,80 @@
+console.log('Hello world') // тут я вывел "привет мир" в консоль
+// console.log("Hello world")
+// console.log(`Hello world`)
+
+// console.log("Let's code")
+
+
+// console.log(2 + 2) // 4
+// console.log(2 - 2) // 0
+// console.log(2 * 2) // 4
+// console.log(2 / 2) // 1
+
+// console.log(2 % 2) // 0
+// console.log(5 % 2) // 1
+// console.log(8 % 3) // 2
+// console.log(9 % 3) // 0
+
+
+// console.log(2 + '2') // 22
+// console.log(2 - '2') // 0
+// console.log(2 * '2') // 4
+// console.log(2 / '2') // 1
+
+// console.log(2 + 'a') // 2a
+// console.log(2 - 'a') // NaN
+// console.log(2 * 'a') // NaN
+// console.log(2 / 'a') // NaN
+
+
+
+// console.log(typeof 100)
+// console.log(typeof '100')
+// console.log(typeof NaN)
+
+
+
+// let
+// let box1 = 'cat'
+// console.log(box1)
+
+// let box2 = 'potato'
+// console.log(box2)
+
+// let name = 'Adilet'
+// let age = 20
+// let job = 'JavaScript instructor'
+// console.log(name, age, job)
+
+
+
+// var
+// box = 'cat'
+// var box
+// console.log(box)
+
+// var box = 'potato'
+// console.log(box)
+
+// var name = 'Adilet'
+// var age = 20
+// var job = 'JavaScript instructor'
+// console.log(name, age, job)
+
+// const
+// const box = 'cat'
+// console.log(box)
+
+// const race = 'Mongoloid'
+// const name = 'Adilet'
+// const age = 20
+// const job = 'JavaScript instructor'
+// console.log(name, age, job)
+
+
+// const name_of_teacher = 'Adilet'
+// const nameOfTeacher = 'Adilet'
+
+// const box1 = 'cat'
+// const box2 = 'cat'
+
