c2e4031a489207d93ed3288f55801fef90fab976
Robert <you@example.com>
2022-08-08 17:34:46 +0600

    added changes on dev branch

---
+++ 3.js
@@ -0,0 +1,105 @@
+// x = 2, a = 3, b = 5
+// x + a = b
+// let x = 2
+// let a = 3
+// let b = 5
+// console.log(x, '+', a, '=', b)
+
+
+
+// const cat = 'Barsik'
+// let box = cat
+// console.log(box) // Barsik
+// console.log(cat) // Barsik
+
+
+// box = box + 'Mursik'
+// box += 'Mursik'
+// console.log(box) // BarsikMursik
+
+// box = box - 'Mursik'
+// box -= 'Mursik'
+// console.log(box)
+
+
+
+// let a = 10
+// a = a + 5
+// a += 5
+// console.log(a) // a = 20
+
+// a = a - 2
+// a -= 2
+// console.log(a) // a = 16
+
+// a = a / 4
+// a /= 2
+// console.log(a) // a = 2
+
+// a = a * 5
+// a *= 5
+// console.log(a) // a = 50
+
+
+// const number = 2022
+// console.log(number, 'type:', typeof number)
+
+// const courses = 'Neolabs'
+// console.log(courses, 'type:', typeof courses)
+
+
+// const isTurnOn = false
+// const isWindowOpen = true
+// console.log(isTurnOn, 'type:', typeof isTurnOn)
+// console.log(isWindowOpen, 'type:', typeof isWindowOpen)
+
+// console.log(200 + true)
+// console.log(200 - false)
+// console.log(true - 'false')
+
+
+// let a
+// console.log(a, 'type:', typeof a)
+
+
+// let a = null
+// console.log(a, 'type:', typeof a)
+
+
+// console.log(1 + null, 1 - null, 1 * null, 1 / null)
+// console.log(1 + undefined, 1 - undefined, 1 * undefined, 1 / undefined)
+
+// console.log('1' + null, '1' - null, '1' * null, '1' / null)
+// console.log('1' + undefined, '1' - undefined, '1' * undefined, '1' / undefined)
+
+// console.log('a' + null, 'a' - null, 'a' * null, 'a' / null)
+// console.log('a' + undefined, 'a' - undefined, 'a' * undefined, 'a' / undefined)
+
+
+
+// console.log(2 ** 3)
+
+
+// let a = 10
+// a = a + 1
+// a += 1
+// a++
+// console.log(a)
+
+
+// let b = 20
+// b = b - 10
+// b -= 10
+// b--
+// console.log(b)
+
+
+// let c = -10
+// console.log(c, -c)
+// let d = 20
+// console.log(d, -d)
+
+
+// let f = '50'
+// console.log(f, typeof f)
+// console.log(+f + 50)
