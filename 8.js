1b102d9d5e9ba6a27b3658ab0ba8aa75d70f248a
Robert <you@example.com>
2022-08-10 16:34:23 +0600

    poko

---
+++ 8.js
@@ -0,0 +1,144 @@
+// const name1 = 'Adilet'
+// const age1 = 21
+// const gender1 = 'male'
+
+// const name2 = 'Temir'
+// const age2 = 19
+// const gender2 = 'male'
+
+// const person1 = {
+//   name: 'Adilet',
+//   age: 21,
+//   gender: 'Male'
+// }
+
+// const person2 = {
+//   name: 'Temir',
+//   age: 19,
+//   gender: 'Male'
+// }
+
+
+
+// const fruit = 'Banana'
+
+// const fruits = {
+//   0: fruit
+// }
+
+// console.log(fruits)
+
+
+// if (true) {
+//   const fruit = 'Apple'
+//   console.log(fruit)
+// }
+
+// console.log(fruit)
+
+
+
+// const neolabs = {
+//   javascript: '16:00 - 18:00',
+//   javascript: '18:30 - 20:30',
+//   javascript: '9:30 - 11:30',
+// }
+
+// neolabs.javascript = '12:00 - 14:00'
+// neolabs.python = '18:30 - 20:30'
+
+// neolabs.javascript = {
+//   time: '16:00 - 18:00',
+//   room: 'Big room',
+//   students: ['Aibike', 'Aiym', 'Temir']
+// }
+
+// console.log(neolabs)
+// console.log(neolabs.javascript.students[0])
+
+
+
+// const fruits = ['apple', 'banana']
+// fruits.push('Melon')
+
+
+// const person = {
+//   name: 'Adilet',
+//   age: 21,
+//   gender: 'Male'
+// }
+
+// console.log(Object.entries(person))
+
+// const personArr = [
+//   ['name', 'Adilet'],
+//   ['age', '21'],
+//   ['gender', 'Male']
+// ]
+// console.log(Object.fromEntries(personArr))
+
+
+
+// const categoriesCorrect = [
+//   {
+//     name: 'Бытовая техника',
+//     items: [
+//       {
+//         name: 'Холодильник',
+//         price: 30000,
+//         size: '200x80x80',
+//         images: ['url', 'url', 'url']
+//       }
+//     ]
+//   },
+//   {
+//     name: 'Посуда',
+//     items: [
+//       {
+//         name: 'Кружка',
+//         price: 500,
+//         size: '400 ml',
+//         images: ['url', 'url']
+//       },
+//       {
+//         name: 'Тарелка',
+//         price: 400,
+//         size: 'r = 15',
+//         images: ['url', 'url']
+//       }
+//     ]
+//   }
+// ]
+
+// console.log(categoriesCorrect[0].name)
+// console.log(categoriesCorrect[0].items[0].name)
+
+
+// console.log('Категория: ', categoriesCorrect[0].name)
+// console.log('Товары: ', categoriesCorrect[0].items)
+
+
+// const categories = {
+//   'Бытовая техника': ['Холодильник', 'Тостер'],
+//   'Спорт и отдых': ['Мяч', 'Ракетки'],
+//   'Посуда': ['Кружка', 'Тарелка'],
+// }
+
+// console.log('Категория:', Object.keys(categories)[0])
+// console.log('Товары:', Object.values(categories)[0])
+
+
+
+// kahoot.it
+
+
+if (1 > 0) {
+  'odno'
+} else {
+  'drugoe'
+}
+
+1 > 0 ? 'odno' : 'drugoe'
+
+const age = 21
+age >= 18 ? 'Welcome' : null
