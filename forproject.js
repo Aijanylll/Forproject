7c4dd0e1aaf9ecf8757d3aa0cc6906eea25e8477
Aijanyll <luna.j1a@gmail.com>
2022-08-12 13:30:42 +0600

    changed

--- forproject.js
+++ forproject.js
@@ -5,3 +5,16 @@ function repeat(str,n=2) {
     return result
     }  
     console.log(repeat('neolabs',5))
+    async function sum(a,b){
+      // for (let i=0;i<1000000000000;i++){
+      //     if(i==50000000000){
+      //         return a+b
+      //     }
+      // }
+      await setTimeout(()=>{
+          return a+b
+      },3000)
+      return a+b
+  }
+  //console.log(sum(1,2))
+  
